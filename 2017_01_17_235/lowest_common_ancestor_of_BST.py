e2e47c5346b70c587dbe8dac4ee0085168c78373
wk633 <wk633@outlook.com>
2017-01-17 22:19:49 -0800

    完成今日题目

---
+++ 2017_01_17_235/lowest_common_ancestor_of_BST.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        while (root.val - p.val) * (root.val - q.val) > 0:
+            root = (root.left, root.right)[p.val > root.val]
+        return root
