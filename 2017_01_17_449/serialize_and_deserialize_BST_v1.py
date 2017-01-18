7d2442ad7a154a5c2f7378d38898396abfb140ab
wk633 <wk633@outlook.com>
2017-01-18 10:04:05 -0800

    add a solution without using preorder&inorder tree rebuild

---
+++ 2017_01_17_449/serialize_and_deserialize_BST_v1.py
@@ -0,0 +1,36 @@
+# using deque to rebuild tree
+#
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+        """
+        vals = []
+        def preorder(root):
+            if root:
+                vals.append(root.val)
+                preorder(root.left)
+                preorder(root.right)
+        preorder(root)
+        return " ".join(map(str, vals))
+
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        vals = list(map(int, data.split()))
+        def build(minVal, maxVal):
+            if vals and minVal < vals[0] < maxVal:
+                val0 = vals.pop(0)
+                root = TreeNode(val0)
+                root.left = build(minVal, val0)
+                root.right = build(val0,maxVal)
+                return root
+
+        return build(float("-infinity"), float("infinity"))
