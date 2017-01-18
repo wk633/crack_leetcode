1a702cded1472767ff0fff218467b2d145b8ddcc
wk633 <wk633@outlook.com>
2017-01-18 09:03:46 -0800

    保存修改的代码

---
+++ 2017_01_17_105/constructBinaryTree.py
@@ -0,0 +1,15 @@
+
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        if not inorder:
+            return None
+        idx = inorder.index(preorder.pop(0))
+        root = TreeNode(inorder[idx])
+        root.left = self.buildTree(preorder, inorder[:idx])
+        root.right = self.buildTree(preorder, inorder[idx+1:])
+        return root
