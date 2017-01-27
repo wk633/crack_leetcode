1887a0d956c0a0ae32f7641a97fb22707adf8d73
wk633 <wk633@outlook.com>
2017-01-26 23:10:28 -0800

    solution done

--- 2017_01_25_337/houseRobber3.py
+++ 2017_01_25_337/houseRobber3.py
@@ -1,1 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
+class Solution(object):
+    def rob(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        return self.robHelper(root)[1]
+    def robHelper(self, node):
+        if not node:
+            return (0,0)
+        leftSub = self.robHelper(node.left)
+        rightSub = self.robHelper(node.right)
+        return (leftSub[1]+rightSub[1], max(leftSub[1]+rightSub[1], node.val+leftSub[0]+rightSub[0]))
