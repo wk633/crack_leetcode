ab2eb681f1e4a7e84ba095bb6354b9c1923bf97a
wk633 <wk633@outlook.com>
2017-02-04 20:59:50 -0800

    edit a shorter version

--- 2017_01_29_442/findAllDuplicate_v5.py
+++ 2017_01_29_442/findAllDuplicate_v5.py
@@ -4,15 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        N = len(nums)
         rst = []
         for num in nums:
-            if nums[(num % N) -1] <= N:
-                nums[(num % N) -1] += N
+            if nums[abs(num)-1] < 0:
+                rst.append(abs(num))
             else:
-                if num > N:
-                    rst.append(num-N)
-                else:
-                    rst.append(num)
-
+                nums[abs(num)-1] *= -1
         return rst
