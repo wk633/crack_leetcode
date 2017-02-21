a6b74808d59acff8a369810b1a37fb6c3c0bc82c
wk633 <wk633@outlook.com>
2017-02-20 20:09:42 -0800

    add a faster solution

--- 2017_02_20_14/longestCommonPrefix.py
+++ 2017_02_20_14/longestCommonPrefix.py
@@ -1,3 +1,19 @@
+# class Solution(object):
+#     def longestCommonPrefix(self, strs):
+#         """
+#         :type strs: List[str]
+#         :rtype: str
+#         """
+#         if len(strs) == 0:
+#             return ""
+#         result = strs[0]
+#
+#         for string in strs:
+#             while result and string[:len(result)] != result:
+#                 result = result[:-1]
+#         return result
+
+# a faster version by binary search
 class Solution(object):
     def longestCommonPrefix(self, strs):
         """
@@ -6,9 +22,21 @@ class Solution(object):
         """
         if len(strs) == 0:
             return ""
-        result = strs[0]
+        minlength = min([len(item) for item in strs])
+        low, high = 0, minlength
+        while low <= high:
+            mid = low + (high - low) / 2
+            if self.compare(strs, mid):
+                low = mid + 1
+            else:
+                high = mid - 1
+        return strs[0][:min(low,high)]
 
-        for string in strs:
-            while result and string[:len(result)] != result:
-                result = result[:-1]
-        return result
+    def compare(self, strs, end):
+        target = strs[0][:end]
+        for item in strs:
+            if item.startswith(target):
+                continue
+            else:
+                return False
+        return True
